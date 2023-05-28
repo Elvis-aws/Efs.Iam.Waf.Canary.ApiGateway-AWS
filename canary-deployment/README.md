88027570ee89f86ce5af27f285469f06b9af4170
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2023-05-28 15:18:42 +0100

    updates

---
+++ canary-deployment/README.md
@@ -0,0 +1,10 @@
+
+# Run application
+    # virtualenv env
+    # source env/bin/activate
+    # pip3 install -r requirements.txt
+    # pip3 freeze > requirements.txt
+# Lambda Alias
+1. Deploy template with only first version
+2. Update lambda function then add second version
+3. Update Alias routing/weight
--- canary-deployment/src/app.py
+++ canary-deployment/src/app.py
@@ -9,19 +9,30 @@ def lambda_canary(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "message": "This is Canary deployment Testing",
+            "messages": "This is another version",
             "stage_variable": variable
         }),
     }
 
 
 def lambda_prod(event, context):
-    stage_variables = event['stageVariables']
-    # Use stage variables in your Lambda function logic
-    variable = stage_variables['prod_variable']
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "This is Prod deployment Testing",
-            "stage_variable": variable
-        }),
-    }
+    try:
+        stage_variables = event['stageVariables']
+        # Use stage variables in your Lambda function logic
+        variable = stage_variables['prod_variable']
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "This is Prod deployment Testing",
+                "messages": "This is version 1 of my function",
+                "stage_variable": variable
+            }),
+        }
+    except Exception as ex:
+        return {
+            "body": json.dumps({
+                f"{ex}"
+            }),
+        }
+
+
